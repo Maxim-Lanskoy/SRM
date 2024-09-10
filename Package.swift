aa617758ccb9fe062e7e3d4009c0fd1afd4f7804
Maxim-Lanskoy <lanskoyma@gmail.com>
2024-09-10 22:46:02 +0300

    Initial project files structure.

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "SRM",
+    platforms: [
+        .macOS(.v12),
+        .linux
+    ],
+    products: [
+        .executable(
+            name: "srm",
+            targets: ["SRM"]
+        ),
+    ],
+    dependencies: [
+        // Example:
+        // .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "SRM",
+            dependencies: [
+                // Place to add dependencies from external libraries or modules here, if any
+                // Example: .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ],
+            path: "Sources/SRM",
+            resources: [
+                // If having any resources (e.g., config files), it is possible to define them here
+            ]
+        ),
+        .testTarget(
+            name: "SRMTests",
+            dependencies: ["SRM"],
+            path: "Tests/SRMTests"
+        )
+    ]
+)
